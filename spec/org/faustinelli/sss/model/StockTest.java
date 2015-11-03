8722340fc90e9a37c95344f703fd333b27e06181
Muzietto <marco_faustinelli@yahoo.com>
2015-11-03 08:30:38 +0100

    starting to model

---
+++ spec/org/faustinelli/sss/model/StockTest.java
@@ -0,0 +1,10 @@
+package org.faustinelli.sss.model;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by NEOMOBILE on 03/11/2015.
+ */
+public class StockTest extends TestCase {
+
+}
---
+++ src/org/faustinelli/sss/model/Stock.java
@@ -0,0 +1,7 @@
+package org.faustinelli.sss.model;
+
+/**
+ * Created by NEOMOBILE on 03/11/2015.
+ */
+public class Stock {
+}
