67faaadde5c696605fc4126ae36011935a6af6e8
Muzietto <marco_faustinelli@yahoo.com>
2015-11-03 06:46:15 +0100

    java seeds

---
+++ src/org/faustinelli/sss/Main.java
@@ -0,0 +1,8 @@
+package org.faustinelli.sss;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/org/faustinelli/sss/MainTest.java
@@ -0,0 +1,10 @@
+package org.faustinelli.sss;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by NEOMOBILE on 03/11/2015.
+ */
+public class MainTest extends TestCase {
+
+}
