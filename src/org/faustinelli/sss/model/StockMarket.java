bdde7d951c1a9197d7150579a9e74d5b9b35b8bc
Muzietto <marco_faustinelli@yahoo.com>
2015-11-04 06:36:07 +0100

    embryo of a market

---
+++ src/org/faustinelli/sss/model/StockMarket.java
@@ -0,0 +1,10 @@
+package org.faustinelli.sss.model;
+
+import java.util.List;
+import java.util.Set;
+
+public class StockMarket {
+
+    private List<Stock>stocks;
+    //private Set<Dividend>lastDividends;
+}
