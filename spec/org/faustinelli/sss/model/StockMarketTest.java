68af01318c157ebf8e498b463a65c0c688851899
Muzietto <marco_faustinelli@yahoo.com>
2015-11-16 06:42:09 +0100

    starting to shape stock market

--- spec/org/faustinelli/sss/model/StockMarketTest.java
+++ spec/org/faustinelli/sss/model/StockMarketTest.java
@@ -6,11 +6,13 @@ public class StockMarketTest extends TestCase {
 
     public void testMarket() throws Exception {
 
+        StockMarket gbce = StockMarket.GBCE();
 
-    }
+        gbce.trade(Stock.instance("ABC", Stock.Type.COMMON), Trade.Indicator.SELL, Amount.instance(10), 100);
+        gbce.trade(Stock.instance("ZYZ", Stock.Type.PREFERRED), Trade.Indicator.BUY, Amount.instance(20), 50);
+        gbce.trade(Stock.instance("JKL", Stock.Type.PREFERRED), Trade.Indicator.SELL, Amount.instance(30), 30);
 
-    public void testCreateTrace() throws Exception {
-
-
+        assertEquals(new Integer(18), gbce.gbceAllSharesIndex());
+        assertEquals(Amount.instance(10), gbce.tickerPrice(Stock.instance("ABC", Stock.Type.COMMON)));
     }
 }
--- src/org/faustinelli/sss/model/StockMarket.java
+++ src/org/faustinelli/sss/model/StockMarket.java
@@ -1,18 +1,25 @@
 package org.faustinelli.sss.model;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.function.BinaryOperator;
 
 public class StockMarket {
 
-    private static final StockMarket INSTANCE = null;
+    private static final StockMarket GBCE = new StockMarket();
 
     private Set<Stock> stocks;
     // TODO have a Finance handle this
     private Set<Stock.Dividend> lastDividends;
     private Trader trader;
 
-    public static StockMarket THE_MARKET() {
-        return INSTANCE;
+    private StockMarket() {
+        stocks = new HashSet<Stock>();
+        trader = Trader.instance();
+    }
+
+    public static StockMarket GBCE() {
+        return GBCE;
     }
 
     public Trade trade(Stock aStock, Trade.Indicator anIndicator, Amount aPrice, Integer numShares) {
@@ -22,5 +29,20 @@ public class StockMarket {
 
     public Amount tickerPrice(Stock stock) {
         return trader.tickerPrice(stock);
+    }
+
+    public Integer gbceAllSharesIndex() {
+
+        Integer product = stocks
+                .stream()
+                .map(stock -> trader.stockPrice(stock).value())
+                .reduce(1, new BinaryOperator<Integer>() {
+                    @Override
+                    public Integer apply(Integer acc, Integer curr) {
+                        return acc * curr;
+                    }
+                });
+
+        return (int) Math.round(Math.pow((double) product.intValue(), 1.0 / stocks.size()));
     }
 }
