2df719acbadb3f2a4e8afe429e55537553e3190d
Muzietto <marco_faustinelli@yahoo.com>
2015-11-16 06:41:57 +0100

    starting to shape dividends

--- src/org/faustinelli/sss/model/Stock.java
+++ src/org/faustinelli/sss/model/Stock.java
@@ -1,10 +1,13 @@
 package org.faustinelli.sss.model;
 
+import java.util.Optional;
+
 public class Stock {
 
     private final String symbol;
     private final Stock.Type type;
     private final Amount parValue;
+    private final Integer fixedDividendPc = null;
 
     private Stock(String aName, Stock.Type aType, Amount aParValue) {
         symbol = aName.substring(0, 3).toUpperCase();
@@ -24,8 +27,16 @@ public class Stock {
         return aStock.new Dividend(aStock, aDividend);
     }
 
+    public Optional<Dividend> dividend() {
+        return this.type.dividend(this).flatMap(null);
+    }
+
     public Amount parValue() {
         return Amount.instance(parValue.value());
+    }
+
+    public Integer fixedDividendPc() {
+        return new Integer(fixedDividendPc);
     }
 
     @Override
@@ -45,11 +56,24 @@ public class Stock {
 
     // TODO add enum method for fixed dividend
     public enum Type {
-        COMMON, PREFERRED
+        COMMON {
+            @Override
+            Optional<Amount> dividend(Stock stock) {
+                return Optional.empty();
+            }
+        },
+        PREFERRED {
+            @Override
+            Optional<Amount> dividend(Stock stock) {
+                return Optional.of(Amount.instance(stock.parValue().value() * stock.fixedDividendPc()));
+            }
+        };
+
+        abstract Optional<Amount> dividend(Stock stock);
     }
 
     public class Dividend {
-        Stock stock;
+        private Stock stock;
         private Amount dividend;
 
         private Dividend(Stock aStock, Amount aDividend) {
