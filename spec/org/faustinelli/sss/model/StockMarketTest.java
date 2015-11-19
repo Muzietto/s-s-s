48de36c6426c424850a05169b457f95c35e217f5
Muzietto <marco_faustinelli@yahoo.com>
2015-11-19 05:32:22 +0100

    no more gbce index tests along with the rest

--- spec/org/faustinelli/sss/model/StockMarketTest.java
+++ spec/org/faustinelli/sss/model/StockMarketTest.java
@@ -5,15 +5,17 @@ import org.faustinelli.sss.util.Amount;
 
 public class StockMarketTest extends TestCase {
 
-    public void testMarket() throws Exception {
+    public void testTickerPrice() throws Exception {
 
         StockMarket gbce = StockMarket.GBCE();
 
         gbce.trade(Stock.instance("ABC", Stock.Type.COMMON), Trade.Indicator.SELL, Amount.instance(10), 100);
-        gbce.trade(Stock.instance("ZYZ", Stock.Type.PREFERRED), Trade.Indicator.BUY, Amount.instance(20), 50);
+        gbce.trade(Stock.instance("XYZ", Stock.Type.PREFERRED), Trade.Indicator.BUY, Amount.instance(20), 50);
         gbce.trade(Stock.instance("JKL", Stock.Type.PREFERRED), Trade.Indicator.SELL, Amount.instance(30), 30);
 
-        assertEquals(new Integer(18), gbce.gbceAllSharesIndex());
         assertEquals(Amount.instance(10), gbce.tickerPrice(Stock.instance("ABC", Stock.Type.COMMON)));
+        assertEquals(Amount.instance(20), gbce.tickerPrice(Stock.instance("XYZ", Stock.Type.PREFERRED)));
+        assertEquals(Amount.instance(30), gbce.tickerPrice(Stock.instance("XYZ", Stock.Type.PREFERRED)));
+    }
     }
 }
