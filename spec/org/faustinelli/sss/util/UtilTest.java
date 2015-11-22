142ef13a02f5c774b33986b46cacf33252786b55
Muzietto <marco_faustinelli@yahoo.com>
2015-11-22 11:56:13 +0100

    ready to stress GeometricMean class

--- spec/org/faustinelli/sss/util/UtilTest.java
+++ spec/org/faustinelli/sss/util/UtilTest.java
@@ -2,12 +2,24 @@ package org.faustinelli.sss.util;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class UtilTest extends TestCase {
 
     public void testGeometricMean() {
+
         assertEquals(new Integer(2), GeometricMean.value(Arrays.asList(1, 2, 3, 2, 1)));
+
+        List<Integer> list = new ArrayList<Integer>();
+        Integer gazillion = new Integer(100000);
+//        Integer gazillion = new Integer(1000000000);
+        Integer times = 10;
+        for (int i = 0; i < times; i++) {
+            list.add(gazillion);
+        }
+        assertEquals(gazillion, GeometricMean.value(list));
     }
 
     public void testSimpleFraction() {
