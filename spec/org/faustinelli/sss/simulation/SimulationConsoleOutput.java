a3ef9b9ed79d1d28952e6bc0ba6ccc07a5dc4987
Muzietto <marco_faustinelli@yahoo.com>
2015-11-22 16:56:05 +0100

    MINOR STUFF

--- spec/org/faustinelli/sss/simulation/SimulationConsoleOutput.java
+++ spec/org/faustinelli/sss/simulation/SimulationConsoleOutput.java
@@ -13,19 +13,19 @@ public class SimulationConsoleOutput extends SimulationOutput {
     }
 
     @Override
-    public void output(StockMarket gbce, Simulation.RandomizedTradingClock clock, Map<String,
+    public void output(StockMarket market, Simulation.RandomizedTradingClock clock, Map<String,
             Stock> stocks) {
 
         stream.println("*******************************************************");
         stream.println("* Time is: " + clock.lastTick() + "   *");
         for (Stock stock : stocks.values()) {
             stream.println("* -------------- Stock " + stock.toString() + " ----------------- *");
-            stream.println("*                   ticker price: " + gbce.tickerPrice(stock, clock.lastTick()) + "            *");
-            stream.println("*                 dividend yield: " + gbce.dividendYield(stock, clock.lastTick()) + "                *");
-            stream.println("*                      P/E ratio: " + gbce.peRatio(stock, clock.lastTick()) + "                   *");
+            stream.println("*                   ticker price: " + market.tickerPrice(stock, clock.lastTick()) + "            *");
+            stream.println("*                 dividend yield: " + market.dividendYield(stock, clock.lastTick()) + "                *");
+            stream.println("*                      P/E ratio: " + market.peRatio(stock, clock.lastTick()) + "                   *");
         }
         stream.println("* --------------------------------------------------- *");
-        stream.println("*               GBCE all shares index: " + gbce.gbceAllSharesIndex(clock.lastTick()) + "             *");
+        stream.println("*               GBCE all shares index: " + market.gbceAllSharesIndex(clock.lastTick()) + "             *");
         stream.println("******************************************************");
 
     }
